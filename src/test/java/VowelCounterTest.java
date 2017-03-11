fc53e5400ca02d60baf138e5e6fdafa2893418da
G. Chalauri <g.chalauri@mia.gov.ge>
2017-03-11 16:25:50 +0400

    fix char

--- src/test/java/VowelCounterTest.java
+++ src/test/java/VowelCounterTest.java
@@ -1,6 +1,9 @@
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,24 +15,30 @@ public class VowelCounterTest {
     public void averageVowelTest() throws IOException {
         VowelCounter tester = new VowelCounter(); // MyClass is tested
 
-        assertEquals(tester.averageVowel("INPUT.TXT"),buildResult2());
+        assertEquals(tester.averageVowel("INPUT.TXT"), buildResult2());
 
-        assertEquals(tester.averageVowel("INPUT2.TXT"),buildResult());
+        assertEquals(tester.averageVowel("INPUT2.TXT"), buildResult());
+
+
+      /*  Files.write(Paths.get("test-input.txt"), "Giga, who is programmer is working doing aoeas01010".getBytes("utf-8"),
+                StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+
+*/
     }
 
     private String buildResult() {
         StringBuilder builder = new StringBuilder();
-        builder.append("({a, e, o), 10) -> 3.5");
+        builder.append("({a, e, o}, 10) -> 3.5");
         builder.append(System.lineSeparator());
-        builder.append("({i, o), 5) -> 2.0");
+        builder.append("({i, o}, 5) -> 2.0");
         builder.append(System.lineSeparator());
-        builder.append("({i, o), 7) -> 2.0");
+        builder.append("({i, o}, 7) -> 2.0");
         builder.append(System.lineSeparator());
-        builder.append("({i), 2) -> 1.0");
+        builder.append("({i}, 2) -> 1.0");
         builder.append(System.lineSeparator());
-        builder.append("({a, i), 4) -> 2.0");
+        builder.append("({a, i}, 4) -> 2.0");
         builder.append(System.lineSeparator());
-        builder.append("({o), 3) -> 1.0");
+        builder.append("({o}, 3) -> 1.0");
         builder.append(System.lineSeparator());
 
         return builder.toString();
@@ -38,11 +47,11 @@ public class VowelCounterTest {
 
     private String buildResult2() {
         StringBuilder builder = new StringBuilder();
-        builder.append("({a, o), 5) -> 2.0");
+        builder.append("({a, o}, 5) -> 2.0");
         builder.append(System.lineSeparator());
-        builder.append("({a, o), 6) -> 2.5");
+        builder.append("({a, o}, 6) -> 2.5");
         builder.append(System.lineSeparator());
-        builder.append("({a, e), 4) -> 2.0");
+        builder.append("({a, e}, 4) -> 2.0");
         builder.append(System.lineSeparator());
 
 
